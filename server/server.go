33ac0f7d63a6caab28bdf0247676a12f0fabca0d
chuchu1313 <fangyu1313@gmail.com>
2017-09-07 12:03:41 +0800

    Add server app

---
+++ server/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"log"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/go-server", HelloServer)
+	log.Fatal(http.ListenAndServe(":1234", nil))
+}

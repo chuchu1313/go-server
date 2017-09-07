fb33ca066cb87b5703820fb4c108389937ba60f7
chuchu1313 <fangyu1313@gmail.com>
2017-09-07 12:44:00 +0800

    Render results in folder

--- server/server.go
+++ server/server.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"io"
 	"net/http"
-	"log"
 )
 
-// hello world, the web server
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "hello, world!\n")
-}
-
 func main() {
-	http.HandleFunc("/go-server", HelloServer)
-	log.Fatal(http.ListenAndServe(":1234", nil))
+	http.Handle("/", http.FileServer(http.Dir("html"))) // index.html in html folder
+	http.ListenAndServe(":1234", nil)
 }
